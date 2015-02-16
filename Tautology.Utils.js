4dc32a48fdec92e50f4ac9c18a0123a613a8f39b
marvinax <nightvista@gmail.com>
2015-02-16 22:38:16 +0800

    added comments

--- Tautology.Utils.js
+++ Tautology.Utils.js
@@ -44,6 +44,11 @@ Array.permute = function(shape){
 	},[[]])
 }
 
+/**
+ * grid generates the triangles that can directly applied to Three.js
+ * @param  {Array} shape Dimensions of array
+ * @return {Array}       to be generated
+ */
 Array.grid = function(shape){
 	return Array.mesh(shape).slide(2)								// slide over cols
 				.map(function(e){return e.transpose().slide(2)})	// slide over rows
@@ -52,7 +57,12 @@ Array.grid = function(shape){
 				.flatten().map(function(e){return e.toFace3()});
 }
 
-
+/**
+ * Mesh generates an index array with hierarchical structure, that represents
+ * a multi-dimensional matrix.
+ * @param  {Array} shape Dimensions of Array
+ * @return {Array}       to be generated
+ */
 Array.mesh = function(shape){
 	var whole = Array.range(shape.reduce(function(a, b){return a*b}))
 	
@@ -62,6 +72,14 @@ Array.mesh = function(shape){
 				
 }
 
+
+/**
+ * Returns the (sort of) Cartesian Product of two matrix (this and another),
+ * specify the operation over two element with func
+ * @param  {Array} another Another matrix
+ * @param  {Function} func    Operation to be applied
+ * @return {Array}         The outer product of two array
+ */
 Array.prototype.outer = function(another, func){
 	return this.map(function(outerElem){
 		return another.map(function(innerElem){
@@ -70,16 +88,31 @@ Array.prototype.outer = function(another, func){
 	})
 }
 
+/**
+ * Partitioning the array, with keeping the last part with not-enough items
+ * to be partitioned.
+ * @param  {Number} n number of items for each part
+ * @return {Array}   Array to be returned
+ */
 Array.prototype.partition = function(n){
 	return Array.range(Math.ceil(this.length/n)).map(function(e){return e*n})
 				// starting point of each slice, slice including not-enough items will be kept also.
 				.map(function(e){return this.slice(e, e+n)}, this)
 }
 
+/**
+ * Flatten combines the parts of the array together
+ * @return {Array} Flattened array
+ */
 Array.prototype.flatten = function(){
 	return this.reduce(function(prev, curr){return prev.concat(curr)});
 }
 
+/**
+ * Slide over an array with a window and combines windows together.
+ * @param  {Number} n window-length
+ * @return {Array}   Array of windows
+ */
 Array.prototype.slide = function(n){
 	var res = [];
 	for (var i = this.length - n; i >= 0; i--) {
@@ -89,16 +122,20 @@ Array.prototype.slide = function(n){
 	return res.reverse();
 }
 
-Array.prototype.diff = function(){
-	return this.slide(2).map(function(e){return e[1] - e[0]});
-}
-
+/**
+ * Transpose a Matrix, a.k.a an array of arrays
+ * @return {Array} Transposed array
+ */
 Array.prototype.transpose = function(){
 	 return Object.keys(this[0]).map(
 	    function (c) { return this.map(function (r) { return r[c]; }); }.bind(this)
     );
 }
 
+/**
+ * make a 3-ary array into a THREE.Face3 object
+ * @return {THREE.Face3} Face triangle
+ */
 Array.prototype.toFace3 = function(){
 	if (this.length !=3){
 		throw new Error('have to be length of 3');
@@ -107,6 +144,11 @@ Array.prototype.toFace3 = function(){
 	return new THREE.Face3(this[0], this[1], this[2]);
 }
 
+/**
+ * As name implies, change a two-entry array into another
+ * array with one ordinary and one reversed. 
+ * @return {Array} 
+ */
 Array.prototype.tail = function(){
 	return [this[0], this[1].reverse()];
 }
@@ -127,12 +169,6 @@ Array.prototype.add = function(arr){
 	return null;
 }
 
-Array.prototype.selectBy = function(func){
-	return this.filter(function(elem){
-		return func.call(elem.object);
-	});
-}
-
 /**
  * unzipFor only returns the reference of the objects, instead of
  * the deep copied objects.
@@ -141,8 +177,4 @@ Array.prototype.selectBy = function(func){
  */
 Array.prototype.unzipFor = function(property){
 	return this.map(function(elem){return elem[property]});
-}
-
-Object.values = function(obj){
-	return (Object.keys(obj)).map(function(key){return obj[key]});
 }
