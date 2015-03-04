4291f8e02c40a29bbe31213bb258983c02ccb48a
marvinax <nightvista@gmail.com>
2015-03-04 20:47:03 +0800

    any new update?

---
+++ Tautology.Manuever.js
@@ -0,0 +1,9 @@
+Tautology.Manuever = function(manuever){
+	this.manuever = manuever;
+}
+
+Tautology.Manuever.prototype.constructor = Tautology.Manuever;
+
+Tautology.Manuever.prototype.initManuever = function(){
+	
+}
